57f819a1e1e54eb87dc20e875957a8e8384dd076
Ritwik Saha <s.ritwik98@gmail.com>
2019-10-05 18:18:07 +0530

    add small testdata, little changes

--- code2.py
+++ code2.py
@@ -4,9 +4,9 @@ import sys
 import numpy as np
 from numpy.testing import assert_allclose
 
-damping = 0.85  # damping factor
-max_iter = 10  # maximum iterations
-tol = 1e-7  # maximum tolerance between pagerank results of two consequtive iterations
+damping = 0.85
+max_iter = 10
+tol = 1e-7
 
 
 with open(sys.argv[1], "r") as f:
@@ -14,26 +14,35 @@ with open(sys.argv[1], "r") as f:
     n = int(sys.argv[2])
     print(n)
     A = np.zeros((n, n), dtype=float)
-    for src, dst in reader:  # src = source, dst = destination
-        A[int(dst) - 1][int(src) - 1] = 1.0  # populate the A matrix in index (dst, src)
+    for src, dst in reader:
+        A[int(dst) - 1][int(src) - 1] = 1.0
     for i in range(n):
-        if np.all(A[:, i] == 0.0):  # handle dangling nodes
+        if np.all(A[:, i] == 0.0):
             A[:, i] = 1.0 / n
         else:
             A[:, i] = A[:, i] / np.sum(A[:, i])
 
-# create M matrix from A, incorporating damping factor
 M = damping * A + ((1 - damping) / n) * np.ones((n, n), dtype=float)
+M_ = np.zeros((n + 1, n + 1), dtype=float)
+M_.fill((1 - damping) / n)
+M_[n, :].fill(0)
+M_[n, n] = 1.0
+M_[:n, :n] = damping * A
 
-x = np.ones((n,), dtype=float) / n  # create initial pagerank
+x = np.ones((n,), dtype=float) / n
 x_trail = np.copy(x)
+y = np.ones((n + 1,), dtype=float) / n
+y[n] = 1.0
+y_trail = np.copy(y)
 
 for _ in range(max_iter):
-    print(x)
     x = np.inner(M, x_trail)
+    y = np.inner(M_, y_trail)
     if np.max(np.abs(x - x_trail)) <= tol:
         break
     x_trail = x
+    y_trail = y
 
+assert_allclose(x, y[:-1], rtol=0.0, atol=1e-8)
 
 print(x)
